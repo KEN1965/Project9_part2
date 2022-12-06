6bcd23d8da8686460579d2d230b3d88a601793d6
Kenichi Takahama <hammer528@msn.com>
2022-12-07 07:34:57 +0900

    Initial Commit

---
+++ Project9_part2/Project9_part2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project9_part2App.swift
+//  Project9_part2
+//
+//  Created by K.Takahama on R 4/12/07.
+//
+
+import SwiftUI
+
+@main
+struct Project9_part2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
