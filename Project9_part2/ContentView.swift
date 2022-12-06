8924ac2d7319c3c29009c912a755a03e58f244de
Kenichi Takahama <hammer528@msn.com>
2022-12-07 08:23:59 +0900

    Project9_part2「Creative borders and fills using ImagePaint」finish

--- Project9_part2/ContentView.swift
+++ Project9_part2/ContentView.swift
@@ -7,45 +7,15 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffset = -20.0
-    var petalWidth = 100.0
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
-        }
-        
-        return path
-    }
-}
 
 struct ContentView: View {
     //Project9_part2 やっていきやしょう(๑>◡<๑)！！
-    
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
     var body: some View {
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)//手を加えて重なったところは色が抜けるように
-                .fill(.red, style: FillStyle(eoFill: true))
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("orizuru"),sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.3),scale: 0.4), lineWidth: 20)//手を加えていきます Cupsuleに変更
+            .frame(width: 300,height: 200)
 
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-        }
+        
     }
 }
 
